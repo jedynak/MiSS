b1791f37481cdfd919a0eaf87a703bdcb5fa432a
jedynak <jedynak1991@gmail.com>
2014-10-29 17:19:25 -0400

    kolgomov + i - oraz V z X^2

--- MiSS2/lab2.cpp
+++ MiSS2/lab2.cpp
@@ -12,7 +12,7 @@ void sort(int count, mpq_t* T){
     mpq_t tmp;
     int j,k;
     mpq_init(tmp);
-    for(int i=0;i<count/2;++i){
+    for(int i=count/2-1;i>=0;--i){
 	k=i;
 	do{
 	    j=k;
@@ -23,61 +23,46 @@ void sort(int count, mpq_t* T){
 	    mpq_set(tmp,T[j]);
 	    mpq_set(T[j],T[k]);
 	    mpq_set(T[k],tmp);
-	}while(j==k);
+	}while(j!=k);
     }
     for(int i=count-1;i>0;--i){
 	mpq_set(tmp,T[0]);
 	mpq_set(T[0],T[i]);
 	mpq_set(T[i],tmp);
-	k=i;
+	k=0;
 	do{
 	    j=k;
-	    if(2*j+1<count&&mpq_cmp(T[2*j+1],T[k])>0)
+	    if(2*j+1<i&&mpq_cmp(T[2*j+1],T[k])>0)
 		k=2*j+1;
-	    if(2*j+2<count&&mpq_cmp(T[2*j+2],T[k])>0)
+	    if(2*j+2<i&&mpq_cmp(T[2*j+2],T[k])>0)
 		k=2*j+2;
 	    mpq_set(tmp,T[j]);
 	    mpq_set(T[j],T[k]);
 	    mpq_set(T[k],tmp);
-	}while(j==k);
+	}while(j!=k);
     }
-
 }
 
 void cut_str(char str[])
 {
   long int len = strlen(str);
-  --len;
-  bool fractial = false;
-  long int i = len;
-  while(i>0){
-    if(str[i]=='.'){
-	fractial = true;
-	break;
-    }
-    --i;
-  }
-  if (fractial == true){
-      str[len]='\0';
-      while (len>=0&&str[len]=='0'){
-        str[len]='\0';
-        --len;
-    }
-	if ( str[len]=='.' ) str[len]='\0';
-  }
+  str[--len]='\0';
+  while (str[--len]=='0') 
+    str[len]='\0';
+  if ( str[len]=='.' ) str[len]='\0';
 }
 
-int main(){
-    mpq_t *tab,num;
-    int precision;
+int main(int argc, char *argv[]){
+    mpq_t *tab,num,tmp;
+    long unsigned int precision=atoi(argv[1]);
     int count=0;
     const int k=10;
     int Ytab[k]={0};
     int tablen=1024*1024;
     tab=(mpq_t*)malloc(tablen*sizeof(mpq_t));
     mpq_init(num);
-    mpq_t tmp;
     mpq_init(tmp);
+
     while(gmp_scanf("%Qi",num)!=EOF){
 	++count;
 	if(tablen==count){
@@ -89,12 +74,14 @@ int main(){
 	int i=1;
 	while(i<k){
 	    mpq_set_si(tmp,(i*i),100);
-	    if(mpq_cmp(tmp,num)>1)break;
+	    if(mpq_cmp(tmp,num)>0)break;
 	    ++i;
 	}
 	++Ytab[i-1];
     }
-    double V=0;
+    mpq_t V;
+    mpq_init(V);
+    mpq_set_si(V,0,1);
     sort(count,tab);
     mpq_t tmpKminus,tmpKplus,Kminus,Kplus;
     mpq_init(tmpKminus);
@@ -107,36 +94,55 @@ int main(){
     for(int i=1;i<count;++i){
 	mpq_set_si(tmpKminus,-i,count);
 	mpq_add(tmpKminus,tmpKminus,tab[i]);
-	mpq_set_si(tmpKplus,(i-1),count);
+	mpq_set_si(tmpKplus,(i+1),count);
 	mpq_sub(tmpKplus,tmpKplus,tab[i]);
 	if(mpq_cmp(tmpKminus,Kminus)>0)mpq_set(Kminus,tmpKminus);
 	if(mpq_cmp(tmpKplus,Kplus)>0)mpq_set(Kplus,tmpKplus);
     }
+    mpq_t pos,pow;
+    mpq_init(V);
+    mpq_init(pos);
+    mpq_init(pow);
     for(int i=0;i<k;++i){
-	V+=(Ytab[i]-count/k)*(Ytab[i]-count/k)/(count/k);
+	mpq_set_si(pos,count,10);
+	mpq_set_si(pow,Ytab[i],1);
+	mpq_sub(pow,pow,pos);
+	mpq_mul(pow,pow,pow);
+	mpq_div(pow,pow,pos);	
+	//V+=(Ytab[i]-k/count)*(Ytab[i]-k/count)/(k/count);
+	mpq_add(V,V,pow);
     }
-    
+    /*
     int period;
     for(period=1;period<count;++period){
 	int isPeriod=true;
 	for(int j=period;j<count;++j){
-	    if(mpq_cmp(tab[j],tab[j%period])){
+	    if(mpz_cmp(tab[j],tab[j%period])){
 		isPeriod=false;
 		break;
 	    }
 	}
 	if(isPeriod==true)break;
-    }
+    }*/
     
-    char str_avg[36636];
-    
-    //gmp_sprintf(str_avg, "%Qf", avarage);
+    //use gmp_sprintf to create string in order to easier format output???
+    char str_avg[1024*1024];
+    mpf_t printer;
+    mpf_init2(printer,__NUM_BIT);
+    mpf_set_q(printer,Kplus);
+    gmp_sprintf(str_avg, "%.*Ff", precision+1, printer);
     cut_str(str_avg);
-    printf("%s\n", str_avg, strlen(str_avg));
-    
-    //gmp_sprintf(str_avg, "%.*Ff", precision+1, variance);
+    printf("%s\n", str_avg);
+
+    mpf_set_q(printer,Kminus);
+    gmp_sprintf(str_avg, "%.*Ff", precision+1, printer);
     cut_str(str_avg);
-    printf("%s\n", str_avg, strlen(str_avg));
+    printf("%s\n", str_avg);
+
+    mpf_set_q(printer,V);
+    gmp_sprintf(str_avg, "%.*Ff", precision+1, printer);
+    cut_str(str_avg);
+    printf("%s\n", str_avg);
     
     //printf("%d\n",period);
     return 0;
