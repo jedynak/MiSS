b4c879076d86a8b7d90e215a7887a2a4ad7c487f
jedynak <jedynak1991@gmail.com>
2014-12-09 13:18:53 -0500

    uwzglednienie pochodnej z x

--- miss4/miss4.c
+++ miss4/miss4.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <fstream>
 
 double f(double x, double t) //pierwsza pochodna
 {
     return x*x-t;
 }
 
-double fp(double x) //druga pochodna
+double fp(double x, double t) //druga pochodna
 {
     return 2*x;
 }
@@ -14,17 +15,29 @@ double fp(double x) //druga pochodna
 int main()
 {
     double e, x0, x;
+    double xp;	//pochodna x
     double i;
 
+    printf("Wpisz wartosc poczatkowa i krok\n", e);
     scanf("%lf %lf", &x0, &e);
     //printf("   %lf\n", e);
 
     x = x0;
-    for ( i=0; i<1; i+=e )
+    std::ofstream file;
+    file.open ("plot");
+    printf("%lf %lf\n", 0, x);
+    file<<0<<" "<<x<<std::endl;
+    xp = 0; 
+
+    for ( i=e; i<=1; i+=e )
     {
-        x = x - f(x, i)/fp(x);
+        xp = xp - f(x, i)/fp(x, i);	
+        x = x + xp *e;
         printf("%lf %lf\n", i, x);
+        file<<i<<" "<<x<<std::endl;
     }
+    file.close();
+    
 
     return 0;
 }
