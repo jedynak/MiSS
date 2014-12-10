b6152b875bc6ea3b7f8339c9a4b9ca203f0db3f1
jedynak <jedynak1991@gmail.com>
2014-12-10 15:25:46 -0500

    zmiana na runge-kutta

--- miss4/miss4.c
+++ miss4/miss4.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <fstream>
 
-double f(double x, double t) //pierwsza pochodna
+double f(double x, double t) 
 {
-    return x*x-t;
+    return -x*x*x*(t+1);
 }
 
-double fp(double x, double t) //druga pochodna
-{
-    return 2*x;
-}
 
+double runge_kutta3(double x, double t, double h)
+{
+	double k1,k2,k3;
+	k1 = h * f(x,t);
+	k2 = h * f(x + k1/2,t+h/2);
+	k3 = h * f(x-k1+2*k2,t+h);
+	return x + (k1 + 4*k2 + k3)/6;
+}
 
 int main()
 {
     double e, x0, x;
-    double xp;	//pochodna x
     double i;
 
     printf("Wpisz wartosc poczatkowa i krok\n", e);
@@ -27,12 +30,10 @@ int main()
     file.open ("plot");
     printf("%lf %lf\n", 0, x);
     file<<0<<" "<<x<<std::endl;
-    xp = 0; 
 
     for ( i=e; i<=1; i+=e )
-    {
-        xp = xp - f(x, i)/fp(x, i);	
-        x = x + xp *e;
+    {	
+        x = runge_kutta3(x,i,e);
         printf("%lf %lf\n", i, x);
         file<<i<<" "<<x<<std::endl;
     }
